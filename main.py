37c6642c7de47c54946c4cde8ee73cdf743f6acf
yl7690 <yl7690@nyu.edu>
2023-11-12 16:32:25 -0500

    fixed f-value list and extra newline character input

--- main.py
+++ main.py
@@ -14,25 +14,29 @@
 from node import Node
 from graph import AStar
 
+
 def main():
     # VARIABLES
-    INPUT_FILE = "INPUT1.txt"
+    INPUT_FILE = "INPUT2.txt"
     INPUT_DATA = ""
     OUTPUT_DATA = ""
     init_state = [([[], [], []]) for i in range(3)]
     goal_state = [([[], [], []]) for i in range(3)]
-    
+
     # READ INPUT
     with open(INPUT_FILE) as f:
         i = 0
-        for line in f:
+        for j in range(23):
+            line = f.readline()
+            if j == 22:
+                line = line.strip()
             INPUT_DATA += line
             OUTPUT_DATA += line
             line = line.strip()
             read_states(i, line, init_state, goal_state)
             i += 1
         f.close()
-    
+
     print("INITIAL STATE:")
     print_matrix(init_state)
 
@@ -40,7 +44,7 @@ def main():
     actions = ['E', 'W', 'S', 'N', 'U', 'D']
     a_star = AStar(init_state, goal_state, actions)
     found = a_star.search()
-    
+
     print("---------------------\nRESULT")
     if found:
         print("FOUND!")
@@ -50,11 +54,10 @@ def main():
     print("FINAL STATE:")
     print_matrix(a_star.curr_node.state)
     print("Actions:", a_star.curr_node.action)
-    
+
     write_output_file(OUTPUT_DATA, a_star)
     exit()
 
-    
 
 # Simple matrix printing function
 def print_matrix(matrix):
@@ -83,42 +86,41 @@ def read_states(line_num, input_line, init_state, goal_state):
         elif line_num < 19:
             goal_state[1][line_num % 16] = list(map(int, input_line.split()))
         else:
-            goal_state[2][line_num % 20] = list(map(int, input_line.split())) 
-   
+            goal_state[2][line_num % 20] = list(map(int, input_line.split()))
 
-# Get list of f(n) values along the optimal path            
+        # Get list of f(n) values along the optimal path
+
+
 def get_f_value_list(final_node, init_state):
     f_value_list = [final_node.f_value]
-    final_state = final_node.state
     parent_node = final_node.parent
-    
+
     while (parent_node.state != init_state):
         f_value_list.insert(0, parent_node.f_value)
         parent_node = parent_node.parent
-        
+
+    f_value_list.insert(0, parent_node.f_value)
+
     return f_value_list
 
 
 def write_output_file(OUTPUT_DATA, result_graph):
     outf = open("output.txt", "w")
-    
+
     depth_shallowest_goal = result_graph.curr_node.level
     total_num_nodes = len(result_graph.reached)
     actions = result_graph.curr_node.action
     f_value_list = get_f_value_list(result_graph.curr_node, result_graph.init_state)
-    
-    OUTPUT_DATA += "\n\n"                                  # line 24 is blank
-    OUTPUT_DATA += str(depth_shallowest_goal) + '\n'       # line 25 is depth of shallowest goal
-    OUTPUT_DATA += str(total_num_nodes) + '\n'             # line 26 is total number of nodes
-    OUTPUT_DATA += ' '.join(actions) + '\n'                # line 27 is sequence of actions
+
+    OUTPUT_DATA += "\n\n"  # line 24 is blank
+    OUTPUT_DATA += str(depth_shallowest_goal) + '\n'  # line 25 is depth of shallowest goal
+    OUTPUT_DATA += str(total_num_nodes) + '\n'  # line 26 is total number of nodes
+    OUTPUT_DATA += ' '.join(actions) + '\n'  # line 27 is sequence of actions
     OUTPUT_DATA += ' '.join([str(n) for n in f_value_list])
-    
+
     outf.write(OUTPUT_DATA)
     outf.close()
-    
 
-    
-    
 
 if __name__ == "__main__":
     main()

ec6cb527e30f26aa05f140c5a9233ff2d11491bf
thuvu17 <tnv2002@nyu.edu>
2023-11-14 00:34:00 -0500

    functions should be 2 lines apart

--- node.py
+++ node.py
@@ -4,15 +4,17 @@ from argparse import Action
 class Node:
     # Initialize
     def __init__(self, state, parent, action, level, f_value):
-        self.state = state  # Current state
-        self.parent = parent  # Pointer to parent node
-        self.action = action  # List of actions to get to this node
-        self.level = level  # Level of node
+        self.state = state      # Current state
+        self.parent = parent    # Pointer to parent node
+        self.action = action    # List of actions to get to this node
+        self.level = level      # Level of node
         self.f_value = f_value  # f(n)
+
 
     # Comparing two nodes = compare their f_values
     def __lt__(self, other):
         return self.f_value < other.f_value
+
 
     # Printing a node prints its action list, f_value, level, and state
     def __str__(self):
